806a454129fed492c05a278ffc7555893bd4266b
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-01-19 17:25:31 -0800

    This commit added an array of objects (types of Pokemon).

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert('Hello, world');
-let favoriteFood='rockfish';
-document.write(favoriteFood);
+let pokemonList=[
+    {name:'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] }, 
+    {name: 'Charmander', height: 0.6, types: ['fire']}, 
+    {name:'Blastoise', height: 1.6, types: ['water'] }, 
+    {name: 'Beedrill', height: 1, types: ['bug', 'poison']}, 
+    {name: 'Pikachu', height: 0.4, types: ['electric']}, 
+    {name: 'Persian', height: 1, types: ['normal']},
+];
