b5f914c30d70a056e60bc6f1b48759ce3744d400
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-01-19 17:41:05 -0800

    This commit added back an alert necessary to pass the previous exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,16 @@ let pokemonList=[
     {name:'Blastoise', height: 1.6, types: ['water'] }, 
     {name: 'Beedrill', height: 1, types: ['bug', 'poison']}, 
     {name: 'Pikachu', height: 0.4, types: ['electric']}, 
-    {name: 'Persian', height: 1, types: ['normal']},
+    {name: 'Persian', height: 1, types: ['normal']}
 ];
+
+alert('Hello, world');
+let favoriteFood='rockfish';
+document.write(favoriteFood);
+
+document.write(pokemonList[4].types);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    document.write(pokemonList);
+};
