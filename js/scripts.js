9de5337bc9b74c974bff976a40ab70b395df2337
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-01-23 16:53:09 -0800

    This commit adds a while  loop that iterates through each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,81 @@
+//declares an array of pokemon to be used as data
+
 let pokemonList=[
-    {name:'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1.0, types: ['grass', 'poison'] }, 
-    {name: 'Charmander', height: 0.6, types: ['fire']}, 
-    {name:'Blastoise', height: 1.6, types: ['water'] }, 
-    {name: 'Beedrill', height: 1, types: ['bug', 'poison']}, 
-    {name: 'Pikachu', height: 0.4, types: ['electric']}, 
-    {name: 'Persian', height: 1, types: ['normal']}
+    {
+    name:'Bulbasaur', 
+    height: 0.7,
+    types: ['grass', 'poison']
+    },
+    {
+    name: 'Ivysaur', 
+    height: 1.0, 
+    types: ['grass', 'poison'] 
+    }, 
+    {name: 'Charmander', 
+    height: 0.6, 
+    types: ['fire']
+    }, 
+    {
+    name:'Blastoise', 
+    height: 1.6, 
+    types: ['water'] 
+    }, 
+    {
+    name: 'Beedrill', 
+    height: 1, 
+    types: ['bug', 'poison']
+    }, 
+    {
+    name: 'Pikachu', 
+    height: 0.4, 
+    types: ['electric']
+    }, 
+    {
+    name: 'Persian', 
+    height: 1, 
+    types: ['normal']
+    }
 ];
 
-alert('Hello, world');
-let favoriteFood='rockfish';
-document.write(favoriteFood);
+//for loop that iterates over each item in pokemon array and writes name
 
-document.write(pokemonList[4].types);
+let pokemonNameHeight = function( array){
+    for(let i=0; i < array.length; i ++ ){
+        document.write(' ')
+        document.write(`<br> ${array[i].name} height: ${array[i].height}.` )
+        
+    }
+}
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    document.write(pokemonList);
-};
+pokemonNameHeight(pokemonList);
+
+
+document.write('<br>')
+
+
+
+//using while loop to add note to highlight the biggest pokemon in the list 
+let biggestPokemon = function (array){
+    document.write("We're looking for a tall pokemon!");
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].height === 1.6) {
+            document.write(`<br> ${array[i].name + array[i].height}. Wow, that's big!\n`);
+
+        } else {
+            document.write(`<br> ${array[i].name} not the tallest`);
+        }
+    }
+}
+
+biggestPokemon(pokemonList);
+
+
+
+
+
+
+
+
+
+
+
