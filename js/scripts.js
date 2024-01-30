76ae021ee7187c301974ba05e78f77c4cdecbeb6
michaela-wasson <108202135+michaela-wasson@users.noreply.github.com>
2024-01-29 18:36:26 -0800

    Reformatted the array list

--- js/scripts.js
+++ js/scripts.js
@@ -3,40 +3,41 @@
 const pokemonRepository= (function () 
 { let pokemonList =
     [
-    {
-    name:'Bulbasaur', 
-    height: 0.7,
-    types: ['grass', 'poison']
-    },
-    {
-    name: 'Ivysaur', 
-    height: 1.0, 
-    types: ['grass', 'poison'] 
-    }, 
-    {name: 'Charmander', 
-    height: 0.6, 
-    types: ['fire']
-    }, 
-    {
-    name:'Blastoise', 
-    height: 1.6, 
-    types: ['water'] 
-    }, 
-    {
-    name: 'Beedrill', 
-    height: 1, 
-    types: ['bug', 'poison']
-    }, 
-    {
-    name: 'Pikachu', 
-    height: 0.4, 
-    types: ['electric']
-    }, 
-    {
-    name: 'Persian', 
-    height: 1, 
-    types: ['normal']
-    }
+        {
+            name:'Bulbasaur', 
+            height: 0.7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Ivysaur', 
+            height: 1.0, 
+            types: ['grass', 'poison'] 
+        }, 
+        {
+            name: 'Charmander', 
+            height: 0.6, 
+            types: ['fire']
+        }, 
+        {
+            name:'Blastoise', 
+            height: 1.6, 
+            types: ['water'] 
+        }, 
+        {
+            name: 'Beedrill', 
+            height: 1, 
+            types: ['bug', 'poison']
+        }, 
+        {
+            name: 'Pikachu', 
+            height: 0.4, 
+            types: ['electric']
+        }, 
+        {
+            name: 'Persian', 
+            height: 1, 
+            types: ['normal']
+        }
     ]; 
 
     function add(pokemon) {
